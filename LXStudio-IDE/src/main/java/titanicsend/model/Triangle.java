04983af7e303e4b3a86cf8416f7266535ab650a3
Tom Cahill <tom@alto.com>
2022-02-02 14:22:28 -0800

    lol math

--- LXStudio-IDE/src/main/java/titanicsend/model/Triangle.java
+++ LXStudio-IDE/src/main/java/titanicsend/model/Triangle.java
@@ -18,18 +18,14 @@ public class Triangle extends LXModel {
             Spacing delta = new Spacing(edge[0], edge[1], spacing);
             LXPoint currentPoint = new LXPoint(edge[0]);
 
-            int xDirection = edge[1].x > edge[0].x ? 1 : -1;
-            int yDirection = edge[1].y > edge[0].y ? 1 : -1;
-            int zDirection = edge[1].z > edge[0].z ? 1 : -1;
-
             while (currentPoint.x >= Math.min(edge[0].x, edge[1].x) && currentPoint.x <= Math.max(edge[0].x, edge[1].x) &&
                     currentPoint.y >= Math.min(edge[0].y, edge[1].y) && currentPoint.y <= Math.max(edge[0].y, edge[1].y) &&
                     currentPoint.z >= Math.min(edge[0].z, edge[1].z) && currentPoint.z <= Math.max(edge[0].z, edge[1].z)) {
                 points.add(currentPoint);
                 currentPoint = new LXPoint(currentPoint);
-                currentPoint.setX(currentPoint.x + (float) (xDirection * delta.xSpacing));
-                currentPoint.setY(currentPoint.y + (float) (yDirection * delta.ySpacing));
-                currentPoint.setZ(currentPoint.z + (float) (zDirection * delta.zSpacing));
+                currentPoint.setX(currentPoint.x + (float) delta.xSpacing);
+                currentPoint.setY(currentPoint.y + (float) delta.ySpacing);
+                currentPoint.setZ(currentPoint.z + (float) delta.zSpacing);
             }
         }
         return points;
@@ -39,9 +35,9 @@ public class Triangle extends LXModel {
         public double xSpacing, ySpacing, zSpacing;
         public Spacing(LXPoint p1, LXPoint p2, double spacing) {
             double distance = Util.distance(p1, p2);
-            xSpacing = Math.abs((p2.x - p1.x) / distance);
-            ySpacing = Math.abs((p2.y - p1.y) / distance);
-            zSpacing = Math.abs((p2.z - p1.z) / distance);
+            xSpacing = (p2.x - p1.x) / distance;
+            ySpacing = (p2.y - p1.y) / distance;
+            zSpacing = (p2.z - p1.z) / distance;
         }
     }
 
