16c3bbeb1e1ef6328352210efa3df5af0bbd5c39
Tom Cahill <tom@alto.com>
2022-02-06 16:38:24 -0800

    make animation smoother

--- LXStudio-IDE/src/main/java/titanicsend/pattern/tom/Pulse.java
+++ LXStudio-IDE/src/main/java/titanicsend/pattern/tom/Pulse.java
@@ -3,15 +3,12 @@ package titanicsend.pattern.tom;
 import heronarts.lx.LX;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXPoint;
-import heronarts.lx.modulator.SinLFO;
-import heronarts.lx.parameter.CompoundParameter;
-import heronarts.lx.parameter.FunctionalParameter;
-import heronarts.lx.parameter.LXParameter;
+import heronarts.lx.modulator.SawLFO;
+import heronarts.lx.parameter.*;
 import heronarts.lx.transform.LXVector;
 import titanicsend.model.TEPanelModel;
 import titanicsend.model.TEVertex;
 import titanicsend.pattern.TEPattern;
-import titanicsend.util.FloorPoint;
 import titanicsend.util.PanelStriper;
 
 import java.util.*;
@@ -26,7 +23,7 @@ public class Pulse extends TEPattern {
                     .setUnits(LXParameter.Units.HERTZ)
                     .setDescription("Rate of the rotation");
 
-    protected final SinLFO phase = new SinLFO(0, 1, new FunctionalParameter() {
+    protected final SawLFO phase = new SawLFO(0, 1, new FunctionalParameter() {
         public double getValue() {
             return 1000 / rate.getValue();
         }
@@ -45,15 +42,13 @@ public class Pulse extends TEPattern {
             LXPoint[][] panelPoints = pointMap.get(entry.getKey());
             int litIndex = (int) (phase * (panelPoints.length - 1));
             LXPoint[] litSection = panelPoints[litIndex];
-            for (LXPoint point : litSection) {
-                colors[point.index] = LXColor.WHITE;
+
+            for (int i = 0; i < entry.getValue().points.length; i++) {
+                colors[entry.getValue().points[i].index] = LXColor.BLACK;
             }
 
-            for (int i = 0; i < panelPoints.length; i++) {
-                if (i == litIndex) continue;
-                for (LXPoint point : panelPoints[i]) {
-                    colors[point.index] = LXColor.BLACK;
-                }
+            for (LXPoint point : litSection) {
+                colors[point.index] = LXColor.WHITE;
             }
         }
     }
@@ -71,7 +66,6 @@ public class Pulse extends TEPattern {
 
     private LXPoint[][] buildPanelMap(TEPanelModel panel) {
         ArrayList<ArrayList<LXPoint>> points = new ArrayList<ArrayList<LXPoint>>();
-        Set<LXPoint> unassignedPoints = Arrays.stream(panel.points).collect(Collectors.toSet());
 
         TEVertex[] currentVertices = {
                 new TEVertex(new LXVector(panel.v0), -1),
@@ -79,32 +73,27 @@ public class Pulse extends TEPattern {
                 new TEVertex(new LXVector(panel.v2), -1)
         };
 
-        int currentIndex = 0;
-        while (currentVertices[0].distanceTo(panel.centroid) > (10 * PanelStriper.DISTANCE_BETWEEN_PIXELS)) {
-            points.add(new ArrayList<LXPoint>());
-            ArrayList<LXPoint> assignedPoints = new ArrayList<LXPoint>();
+        int i = 1;
+        while (currentVertices[0].distanceTo(panel.centroid) > (2 * PanelStriper.DISTANCE_BETWEEN_PIXELS)) {
+            points.add(0, new ArrayList<LXPoint>());
             LXVector[][] edges = {
                     {currentVertices[0], currentVertices[1]},
                     {currentVertices[1], currentVertices[2]},
                     {currentVertices[0], currentVertices[2]},
             };
 
-            for (LXPoint point : unassignedPoints) {
+            for (LXPoint point : panel.points) {
                 for (LXVector[] edge : edges) {
-                    if (distanceBetPointAndLine(edge[0], edge[1], point) < 5 * PanelStriper.DISTANCE_BETWEEN_PIXELS) {
-                        points.get(currentIndex).add(point);
+                    if (distanceBetweenPointAndLineSegment(edge[0], edge[1], point) < 2 * PanelStriper.DISTANCE_BETWEEN_PIXELS) {
+                        points.get(0).add(point);
                     }
                 }
             }
 
-            for (LXPoint point : points.get(currentIndex)) {
-                unassignedPoints.remove(point);
-            }
-
             for (TEVertex vertex : currentVertices) {
-                vertex.nudgeToward(panel.centroid, (float) 0.05);
+                vertex.nudgeToward(panel.centroid, (float) 0.01 * i);
             }
-            currentIndex++;
+            i++;
         }
 
         return points.stream()
@@ -112,14 +101,19 @@ public class Pulse extends TEPattern {
                 .toArray(LXPoint[][]::new);
     }
 
-    private static double distanceBetPointAndLine(LXVector line_p1, LXVector line_p2, LXPoint p) {
+    private static double distanceBetweenPointAndLineSegment(LXVector line_p1, LXVector line_p2, LXPoint p) {
         LXVector v0 = new LXVector(p);
         LXVector v1 = new LXVector(line_p1);
         LXVector v2 = new LXVector(line_p2);
-        double numerator = new LXVector(v0).sub(v1).cross(new LXVector(v0).sub(v2)).mag();
-        double denominator = new LXVector(v2).sub(v1).mag();
 
-        return numerator / denominator;
+        double segmentLengthSquared = new LXVector(v2).sub(v1).magSq();
+        double t = Math.max(
+                0,
+                Math.min(1, new LXVector(v0).sub(v1).dot(new LXVector(v2).sub(v1)) / segmentLengthSquared)
+        );
+        LXVector projection = new LXVector(v1).add(new LXVector(v2).sub(v1).mult((float)t));
+
+        return v0.dist(projection);
     }
 
 }
