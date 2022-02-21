76783ec59f32a6f5e8972aad43771b3a1ba458b0
Alex Toombs <alex.m.toombs@gmail.com>
2022-02-20 20:43:48 -0800

    Down once more

--- LXStudio-IDE/src/main/java/titanicsend/app/OSCListener.java
+++ LXStudio-IDE/src/main/java/titanicsend/app/OSCListener.java
@@ -1,6 +1,8 @@
 package titanicsend.app;
 
 import heronarts.lx.LX;
+import heronarts.lx.mixer.LXAbstractChannel;
+import heronarts.lx.mixer.LXChannel;
 import heronarts.lx.osc.LXOscListener;
 import heronarts.lx.osc.OscMessage;
 
@@ -11,24 +13,20 @@ import titanicsend.pattern.mike.Checkers;
 public class OSCListener implements LXOscListener {
     private LX lx;
     private TEWholeModel model;
-    private String currentClassName;
+    private int channelIndex;
 
     public OSCListener(LX lx, TEWholeModel model) {
         this.lx = lx;
         this.model = model;
-        this.currentClassName = Bubbles.class.getName();
-        lx.instantiatePattern(this.currentClassName);
+        this.channelIndex = 0;
     }
 
     public void oscMessage(OscMessage message) {
-        if (this.currentClassName == Bubbles.class.getName()) {
-            this.currentClassName = Checkers.class.getName();
-        } else {
-            this.currentClassName = Bubbles.class.getName();
-        }
-
+        this.channelIndex = this.channelIndex + 1;
         try {
-            lx.instantiatePattern(this.currentClassName);
+            LXAbstractChannel abstractChannel = lx.engine.mixer.channels.get(0);
+            LXChannel channel = (LXChannel)abstractChannel;
+            channel.focusedPattern.setValue(channelIndex);
         } catch (Exception x ) {
             LX.error("Error instantiating pattern: " + x.toString());
         }
