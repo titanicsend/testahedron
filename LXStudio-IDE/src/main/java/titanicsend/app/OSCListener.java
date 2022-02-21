0ecd0ed53dfdd7b469e2ba05f34e59051645ccb4
Alex Toombs <alex.m.toombs@gmail.com>
2022-02-20 20:21:25 -0800

    OSC: One more debugging step

--- LXStudio-IDE/src/main/java/titanicsend/app/OSCListener.java
+++ LXStudio-IDE/src/main/java/titanicsend/app/OSCListener.java
@@ -11,21 +11,27 @@ import titanicsend.pattern.mike.Checkers;
 public class OSCListener implements LXOscListener {
     private LX lx;
     private TEWholeModel model;
-    private string currentClassName;
+    private String currentClassName;
 
     public OSCListener(LX lx, TEWholeModel model) {
         this.lx = lx;
         this.model = model;
-        this.currentClassName = Bubbles.class;
+        this.currentClassName = Bubbles.class.getName();
         lx.instantiatePattern(this.currentClassName);
     }
 
     public void oscMessage(OscMessage message) {
-        if (this.currentClassName == Bubbles.class) {
-            this.currentClassName = Checkers.class;
+        if (this.currentClassName == Bubbles.class.getName()) {
+            this.currentClassName = Checkers.class.getName();
+        } else {
+            this.currentClassName = Bubbles.class.getName();
         }
 
-        lx.instantiatePattern(this.currentClassName);
+        try {
+            lx.instantiatePattern(this.currentClassName);
+        } catch (Exception x ) {
+            LX.error("Error instantiating pattern: " + x.toString());
+        }
         LX.log(message.toString());
     }
 }
