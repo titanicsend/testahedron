986410ea4a4de2645dc3eeca9c62409c2343f404
Alex Toombs <alex.m.toombs@gmail.com>
2022-02-20 21:24:51 -0800

    Scrap a lot of stuff, but get the demo working

--- LXStudio-IDE/src/main/java/titanicsend/app/OSCListener.java
+++ LXStudio-IDE/src/main/java/titanicsend/app/OSCListener.java
@@ -3,33 +3,38 @@ package titanicsend.app;
 import heronarts.lx.LX;
 import heronarts.lx.mixer.LXAbstractChannel;
 import heronarts.lx.mixer.LXChannel;
+import heronarts.lx.pattern.LXPattern;
 import heronarts.lx.osc.LXOscListener;
 import heronarts.lx.osc.OscMessage;
 
 import titanicsend.model.TEWholeModel;
-import titanicsend.pattern.mike.Bubbles;
-import titanicsend.pattern.mike.Checkers;
 
 public class OSCListener implements LXOscListener {
     private LX lx;
     private TEWholeModel model;
-    private int channelIndex;
+    private long lastSeconds;
 
     public OSCListener(LX lx, TEWholeModel model) {
         this.lx = lx;
         this.model = model;
-        this.channelIndex = 0;
+        this.lastSeconds = System.currentTimeMillis() / 1000l;
     }
 
     public void oscMessage(OscMessage message) {
-        this.channelIndex = this.channelIndex + 1;
+        LX.log(String.format("Osc Message received: %s", message.toString()));
+
+        long seconds = System.currentTimeMillis() / 1000l;
+        if (seconds - this.lastSeconds <= 1) {
+            return;
+        }
+        this.lastSeconds = System.currentTimeMillis() / 1000l;
         try {
             LXAbstractChannel abstractChannel = lx.engine.mixer.channels.get(0);
             LXChannel channel = (LXChannel)abstractChannel;
-            channel.focusedPattern.setValue(channelIndex);
+
+            channel.goRandomPattern();
         } catch (Exception x ) {
-            LX.error("Error instantiating pattern: " + x.toString());
+            LX.error("Error instantiating random pattern: " + x.toString());
         }
-        LX.log(message.toString());
     }
 }
